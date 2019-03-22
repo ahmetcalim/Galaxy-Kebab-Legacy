428bd066639334f0c52a0ce075d8cce74ad71f6b
ahmetcalim <ahmet@pandorareality.com>
2019-03-22 10:26:11 +0300

    Initial commit

---
+++ Assets/DEV/Scripts/GameEventManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventManager : MonoBehaviour
+{
+    public static UnityEvent OnSpiceUp;
+    public static UnityEvent OnGrab;
+    public static UnityEvent OnRelease;
+    public static UnityEvent OnControllerShake;
+}
---
+++ Assets/RepomaticBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepomaticBehaviour : MonoBehaviour
+{
+    public GameObject lavasPrefab;
+    public GameObject durumPrefab;
+    private bool canStart = false;
+    public Transform durumPoint;
+    private GameObject durumInstance;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Lavas")
+        {
+            canStart = true;
+            durumInstance = Instantiate(durumPrefab, transform.position, Quaternion.identity);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (canStart)
+        {
+            durumInstance.transform.position = Vector3.MoveTowards(durumInstance.transform.position, durumPoint.position, .0002f);
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        canStart = false;
+    }
+    public void ThrowDurum()
+    {
+        durumInstance.AddComponent<Rigidbody>();
+        durumInstance.GetComponent<Rigidbody>().AddForce(Vector3.up *3f, ForceMode.Impulse);
+        durumInstance.GetComponent<Rigidbody>().AddForce(Vector3.left * 2f, ForceMode.Impulse);
+    }
+}
